3625db93679b95536c9e80a54795be1db0ac2f83
hawa130 <handanwanga@hotmail.com>
2025-03-20 17:28:35 +0800

    feat: add svgr/webpack

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,7 @@ import { dirname } from 'path'
 import { fileURLToPath } from 'url'
 
 import { FlatCompat } from '@eslint/eslintrc'
+import { defineConfig, globalIgnores } from 'eslint/config'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
@@ -10,6 +11,9 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 })
 
-const eslintConfig = [...compat.extends('next/core-web-vitals', 'next/typescript')]
+const eslintConfig = defineConfig([
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  globalIgnores(['svgr.d.ts', 'node_modules/', '.next/']),
+])
 
 export default eslintConfig
