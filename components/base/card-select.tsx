dc383abb70b4d929a3cae37f7171ff9403aa549c
hawa130 <handanwanga@hotmail.com>
2025-03-20 17:29:39 +0800

    feat: add card select component

---
+++ components/base/card-select.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react'
+import * as RadioGroupPrimitive from '@radix-ui/react-radio-group'
+
+import { cn } from '@/lib/utils'
+
+export function CardSelectGroup({ className, ...props }: React.ComponentProps<typeof RadioGroupPrimitive.Root>) {
+  return <RadioGroupPrimitive.Root className={cn('grid grid-cols-2 gap-3', className)} {...props} />
+}
+
+export function CardSelectItem({ className, ...props }: React.ComponentProps<typeof RadioGroupPrimitive.Item>) {
+  return (
+    <RadioGroupPrimitive.Item
+      className={cn(
+        'group bg-card aria-checked:border-primary aria-checked:bg-primary/5 hover-enabled:border-accent-foreground/25 hover-enabled:bg-accent disabled-all:opacity-50 disabled-all:grayscale-100 not-disabled-all:pointer-events-auto relative flex flex-col items-center rounded-lg border p-4 transition-colors disabled:pointer-events-none',
+        className,
+      )}
+      {...props}
+    />
+  )
+}
