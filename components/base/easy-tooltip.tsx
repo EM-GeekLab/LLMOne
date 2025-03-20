c9abef65542dbf7355819642d6ac642d58927b22
hawa130 <handanwanga@hotmail.com>
2025-03-20 17:29:27 +0800

    feat: add easy tooltip component and update style

---
+++ components/base/easy-tooltip.tsx
@@ -0,0 +1,32 @@
+import { ComponentProps, ReactNode } from 'react'
+
+import { Tooltip, TooltipContent, TooltipTrigger } from '@/components/ui/tooltip'
+
+export function EasyTooltip({
+  open,
+  defaultOpen,
+  onOpenChange,
+  delayDuration,
+  disableHoverableContent,
+  asChild,
+  children,
+  content,
+  ...props
+}: ComponentProps<typeof Tooltip> &
+  Pick<ComponentProps<typeof TooltipTrigger>, 'asChild'> &
+  ComponentProps<typeof TooltipContent> & {
+    content?: ReactNode
+  }) {
+  return (
+    <Tooltip
+      open={open}
+      defaultOpen={defaultOpen}
+      onOpenChange={onOpenChange}
+      delayDuration={delayDuration}
+      disableHoverableContent={disableHoverableContent}
+    >
+      <TooltipTrigger asChild={asChild}>{children}</TooltipTrigger>
+      <TooltipContent {...props}>{content}</TooltipContent>
+    </Tooltip>
+  )
+}
