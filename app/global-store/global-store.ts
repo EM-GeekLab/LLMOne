81b62c73360b2cd27211600140a4246ce93c055b
hawa130 <handanwanga@hotmail.com>
2025-03-23 05:00:17 +0800

    feat: integrate immer for state management

--- app/global-store/global-store.ts
+++ app/global-store/global-store.ts
@@ -1,3 +1,4 @@
+import { immer } from 'zustand/middleware/immer'
 import { createStore } from 'zustand/vanilla'
 
 import type { ConnectMode, DeployMode } from './types'
@@ -5,26 +6,56 @@ import type { ConnectMode, DeployMode } from './types'
 export type GlobalState = {
   connectMode?: ConnectMode
   deployMode?: DeployMode
+  packagePaths: {
+    systemImagePath?: string
+    environmentPath?: string
+    modelPath?: string
+  }
 }
 
 export type GlobalActions = {
   setConnectMode: (mode?: ConnectMode) => void
   setDeployMode: (mode?: DeployMode) => void
+  setSystemImagePath: (path?: string) => void
+  setEnvironmentPath: (path?: string) => void
+  setModelPath: (path?: string) => void
 }
 
 export type GlobalStore = GlobalState & GlobalActions
 
-export const defaultGlobalState: GlobalState = {}
+export const defaultGlobalState: GlobalState = {
+  packagePaths: {},
+}
 
 export const createGlobalStore = (
   initState = defaultGlobalState,
   listener?: (state: GlobalStore, prev: GlobalState) => void,
 ) => {
-  const store = createStore<GlobalStore>()((set) => ({
-    ...initState,
-    setConnectMode: (mode) => set(() => ({ connectMode: mode })),
-    setDeployMode: (mode) => set(() => ({ deployMode: mode })),
-  }))
+  const store = createStore<GlobalStore>()(
+    immer((set) => ({
+      ...initState,
+      setConnectMode: (mode) =>
+        set((state) => {
+          state.connectMode = mode
+        }),
+      setDeployMode: (mode) =>
+        set((state) => {
+          state.deployMode = mode
+        }),
+      setSystemImagePath: (path) =>
+        set((state) => {
+          state.packagePaths.systemImagePath = path
+        }),
+      setEnvironmentPath: (path) =>
+        set((state) => {
+          state.packagePaths.environmentPath = path
+        }),
+      setModelPath: (path) =>
+        set((state) => {
+          state.packagePaths.modelPath = path
+        }),
+    })),
+  )
   if (listener) {
     store.subscribe(listener)
   }
