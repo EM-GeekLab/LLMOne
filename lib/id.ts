5ba52b9f15d936892d2b6f88930db07adf9efa53
hawa130 <handanwanga@hotmail.com>
2025-03-24 06:57:45 +0800

    feat: add findById utility

--- lib/id.ts
+++ lib/id.ts
@@ -1,3 +1,7 @@
 import { nanoid } from 'nanoid'
 
 export const generateId = nanoid
+
+export function findById<T extends { id: string }>(id: string, list: T[]) {
+  return list.find((item) => item.id === id)
+}
