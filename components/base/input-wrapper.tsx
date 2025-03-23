bf0c0c910d36b53c4389ae8deeec153350506c09
hawa130 <handanwanga@hotmail.com>
2025-03-23 22:44:54 +0800

    feat: add input-wrapper and password-input component

---
+++ components/base/input-wrapper.tsx
@@ -0,0 +1,19 @@
+import { ComponentProps } from 'react'
+import { Slot } from '@radix-ui/react-slot'
+
+import { cn } from '@/lib/utils'
+
+export function InputWrapper({ className, asChild, ...props }: ComponentProps<'label'> & { asChild?: boolean }) {
+  const Comp = asChild ? Slot : 'label'
+  return (
+    <Comp
+      className={cn(
+        '[&_input]:placeholder:text-muted-foreground [&_input]:selection:bg-primary [&_input]:selection:text-primary-foreground dark:bg-input/30 border-input bg-background hover:border-accent-foreground/25 hover:bg-accent focus-within:hover:bg-background focus-within:hover:dark:bg-input/30 focus-within:hover:border-ring flex h-9 w-full min-w-0 cursor-text items-stretch overflow-hidden rounded-md border px-3 text-base shadow-xs transition-[color,box-shadow,border-color,background-color] outline-none has-[input:disabled]:pointer-events-none has-[input:disabled]:cursor-not-allowed has-[input:disabled]:opacity-50 md:text-sm [&_input]:flex-1 [&_input]:bg-transparent [&_input]:py-1 [&_input]:outline-none',
+        'focus-within:border-ring focus-within:ring-ring/50 focus-within:ring-[3px]',
+        'aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive',
+        className,
+      )}
+      {...props}
+    />
+  )
+}
---
+++ components/base/password-input.tsx
@@ -0,0 +1,28 @@
+'use client'
+
+import { ComponentProps, useState } from 'react'
+import { EyeIcon, EyeOffIcon } from 'lucide-react'
+
+import { cn } from '@/lib/utils'
+import { InputWrapper } from '@/components/base/input-wrapper'
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+
+export function PasswordInput({ className, ...props }: ComponentProps<typeof Input>) {
+  const [visible, setVisible] = useState(false)
+
+  return (
+    <InputWrapper className={cn('relative pr-8', className)}>
+      <input type={visible ? 'text' : 'password'} autoComplete="new-password" {...props} />
+      <Button
+        size="icon"
+        variant="ghost"
+        data-state={visible ? 'on' : 'off'}
+        className="data-[state=on]:text-primary text-muted-foreground hover:text-accent-foreground absolute top-1/2 right-1.5 size-6 -translate-y-1/2 p-0 [&_svg:not([class*='size-'])]:size-3.5"
+        onClick={() => setVisible((v) => !v)}
+      >
+        {visible ? <EyeIcon /> : <EyeOffIcon />}
+      </Button>
+    </InputWrapper>
+  )
+}
