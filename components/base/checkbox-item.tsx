1c379840e043c613738edda2d5b6e1c612058c9a
hawa130 <handanwanga@hotmail.com>
2025-03-25 06:35:53 +0800

    feat: add CheckboxItem component and adjust spacing in RadioItem

--- components/base/checkbox-item.tsx
+++ components/base/checkbox-item.tsx
@@ -1,17 +1,17 @@
 import { ComponentProps, useId } from 'react'
 
 import { cn } from '@/lib/utils'
+import { Checkbox } from '@/components/ui/checkbox'
 import { Label } from '@/components/ui/label'
-import { RadioGroupItem } from '@/components/ui/radio-group'
 
-export function RadioItem({ className, children, ...props }: ComponentProps<typeof RadioGroupItem>) {
+export function CheckboxItem({ className, children, ...props }: ComponentProps<typeof Checkbox>) {
   const id = useId()
   const radioId = props.id || `radio-${id}`
 
   return (
     <div className={cn('flex items-center gap-2', className)}>
-      <RadioGroupItem id={radioId} {...props} />
-      <Label className="cursor-pointer font-normal" htmlFor={radioId}>
+      <Checkbox id={radioId} {...props} />
+      <Label className="cursor-pointer" htmlFor={radioId}>
         {children}
       </Label>
     </div>
