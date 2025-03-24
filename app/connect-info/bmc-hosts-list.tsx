32db522e5bd4e7a476f6ca7dc656275426cc6ba3
hawa130 <handanwanga@hotmail.com>
2025-03-25 06:39:55 +0800

    feat: improve BMC and SSH lists

--- app/connect-info/bmc-hosts-list.tsx
+++ app/connect-info/bmc-hosts-list.tsx
@@ -1,11 +1,12 @@
 'use client'
 
-import { EditIcon, PlusIcon } from 'lucide-react'
+import { EditIcon, PlusIcon, Trash2Icon } from 'lucide-react'
 
 import { AppCardSection, AppCardSectionHeader, AppCardSectionTitle } from '@/components/app/app-card'
-import { BmcFormDialog, BmcFormDialogTrigger } from '@/app/connect-info/bmc-form-dialog'
+import { Button } from '@/components/ui/button'
 import { useGlobalStore } from '@/stores'
 
+import { BmcFormDialog, BmcFormDialogTrigger } from './bmc-form-dialog'
 import { DefaultCredentialsConfig } from './default-credentials-config'
 
 export function BmcHostsList() {
@@ -17,35 +18,78 @@ export function BmcHostsList() {
           <AppCardSectionTitle>主机列表</AppCardSectionTitle>
         </AppCardSectionHeader>
         <HostsList />
-        <BmcFormDialog>
-          <div className="flex items-center">
+        <div className="flex items-center gap-2">
+          <BmcFormDialog>
             <BmcFormDialogTrigger>
               <PlusIcon />
               添加主机
             </BmcFormDialogTrigger>
-          </div>
-        </BmcFormDialog>
+          </BmcFormDialog>
+        </div>
       </AppCardSection>
     </>
   )
 }
 
 function HostsList() {
+  const useDefaultCredentials = useGlobalStore((s) => s.defaultCredentials.enabled)
   const hosts = useGlobalStore((s) => s.bmcHosts)
 
   return (
-    <div>
+    <div className="grid grid-cols-[1fr_1fr_1fr_74px] rounded-md border">
+      <div className="text-muted-foreground col-span-full grid grid-cols-subgrid items-center border-b *:px-3 *:py-1.5 *:font-medium">
+        <div>IP</div>
+        <div>用户名</div>
+        <div>密码</div>
+        <div>操作</div>
+      </div>
       {hosts.map((host) => (
-        <div key={host.id} className="flex items-center gap-2">
-          <span>{host.ip}</span>
-          <span>{host.username}</span>
-          <BmcFormDialog id={host.id}>
-            <BmcFormDialogTrigger className="size-6 p-0">
-              <EditIcon />
-            </BmcFormDialogTrigger>
-          </BmcFormDialog>
+        <div
+          key={host.id}
+          className="col-span-full grid grid-cols-subgrid items-center not-last:border-b *:not-last:py-2.5 *:not-last:pl-3"
+        >
+          <div>{host.ip}</div>
+          <div>
+            {host.username || (
+              <span className="text-muted-foreground">{useDefaultCredentials ? '默认' : '无用户名'}</span>
+            )}
+          </div>
+          <div>
+            {host.password ? (
+              '已设置'
+            ) : (
+              <span className="text-muted-foreground">{useDefaultCredentials ? '默认' : '未设置'}</span>
+            )}
+          </div>
+          <div className="flex items-center gap-0.5 px-2">
+            <BmcFormDialog id={host.id}>
+              <BmcFormDialogTrigger variant="ghost" className="size-7 !p-0">
+                <EditIcon className="text-primary size-3.5" />
+                <span className="sr-only">编辑</span>
+              </BmcFormDialogTrigger>
+            </BmcFormDialog>
+            <RemoveButton id={host.id} />
+          </div>
         </div>
       ))}
     </div>
+  )
+}
+
+function RemoveButton({ id }: { id: string }) {
+  const removeHost = useGlobalStore((s) => s.removeBmcHost)
+
+  return (
+    <Button
+      variant="ghost"
+      className="size-7 !p-0"
+      onClick={(e) => {
+        e.stopPropagation()
+        removeHost(id)
+      }}
+    >
+      <Trash2Icon className="text-destructive size-3.5" />
+      <span className="sr-only">删除</span>
+    </Button>
   )
 }
--- app/connect-info/ssh-hosts-list.tsx
+++ app/connect-info/ssh-hosts-list.tsx
@@ -1,12 +1,14 @@
 'use client'
 
-import { EditIcon, PlusIcon } from 'lucide-react'
+import { EditIcon, PlusIcon, Trash2Icon } from 'lucide-react'
+import { match } from 'ts-pattern'
 
 import { AppCardSection, AppCardSectionHeader, AppCardSectionTitle } from '@/components/app/app-card'
-import { SshFormDialog, SshFormDialogTrigger } from '@/app/connect-info/ssh-form-dialog'
+import { Button } from '@/components/ui/button'
 import { useGlobalStore } from '@/stores'
 
 import { DefaultCredentialsConfig } from './default-credentials-config'
+import { SshFormDialog, SshFormDialogTrigger } from './ssh-form-dialog'
 
 export function SshHostsList() {
   return (
@@ -17,35 +19,97 @@ export function SshHostsList() {
           <AppCardSectionTitle>主机列表</AppCardSectionTitle>
         </AppCardSectionHeader>
         <HostsList />
-        <SshFormDialog>
-          <div className="flex items-center">
+        <div className="flex items-center gap-2">
+          <SshFormDialog>
             <SshFormDialogTrigger>
               <PlusIcon />
               添加主机
             </SshFormDialogTrigger>
-          </div>
-        </SshFormDialog>
+          </SshFormDialog>
+        </div>
       </AppCardSection>
     </>
   )
 }
 
 function HostsList() {
+  const useDefaultCredentials = useGlobalStore((s) => s.defaultCredentials.enabled)
   const hosts = useGlobalStore((s) => s.sshHosts)
 
   return (
-    <div>
+    <div className="grid grid-cols-[1fr_80px_1fr_1fr_1fr_74px] rounded-md border">
+      <div className="text-muted-foreground col-span-full grid grid-cols-subgrid items-center border-b *:px-3 *:py-1.5 *:font-medium">
+        <div>IP</div>
+        <div>端口</div>
+        <div>用户名</div>
+        <div>凭据</div>
+        <div>BMC IP</div>
+        <div>操作</div>
+      </div>
       {hosts.map((host) => (
-        <div key={host.id} className="flex items-center gap-2">
-          <span>{host.ip}</span>
-          <span>{host.username}</span>
-          <SshFormDialog id={host.id}>
-            <SshFormDialogTrigger className="size-6 p-0">
-              <EditIcon />
-            </SshFormDialogTrigger>
-          </SshFormDialog>
+        <div
+          key={host.id}
+          className="col-span-full grid grid-cols-subgrid items-center not-last:border-b *:not-last:py-2.5 *:not-last:pl-3"
+        >
+          <div>{host.ip}</div>
+          <div>{host.port}</div>
+          <div>
+            {!(useDefaultCredentials || host.username) ? (
+              <span className="text-muted-foreground">无用户名</span>
+            ) : (
+              host.username || <span className="text-muted-foreground">默认</span>
+            )}
+          </div>
+          <div>
+            {match(host.credentialType)
+              .with('key', () =>
+                host.privateKey ? (
+                  '密钥已设置'
+                ) : (
+                  <span className="text-muted-foreground">{useDefaultCredentials ? '默认' : '未设置密钥'}</span>
+                ),
+              )
+              .with('password', () =>
+                host.password ? (
+                  '密码已设置'
+                ) : (
+                  <span className="text-muted-foreground">{useDefaultCredentials ? '默认' : '未设置密码'}</span>
+                ),
+              )
+              .otherwise(() => (
+                <span className="text-muted-foreground">{useDefaultCredentials ? '默认' : '未设置'}</span>
+              ))}
+          </div>
+          <div>{host.bmcIp}</div>
+          <div className="flex items-center gap-0.5 px-2">
+            <SshFormDialog id={host.id}>
+              <SshFormDialogTrigger variant="ghost" className="size-7 !p-0">
+                <EditIcon className="text-primary size-3.5" />
+                <span className="sr-only">编辑</span>
+              </SshFormDialogTrigger>
+            </SshFormDialog>
+            <RemoveButton id={host.id} />
+          </div>
         </div>
       ))}
     </div>
+  )
+}
+
+function RemoveButton({ id }: { id: string }) {
+  const removeHost = useGlobalStore((s) => s.removeSshHost)
+
+  return (
+    <Button
+      variant="ghost"
+      className="size-7 !p-0"
+      onClick={(e) => {
+        e.stopPropagation()
+        removeHost(id)
+      }}
+    >
+      <Trash2Icon className="text-destructive size-3.5" />
+      <span className="sr-only">删除</span>
+    </Button>
   )
 }
