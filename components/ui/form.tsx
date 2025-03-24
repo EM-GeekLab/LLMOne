563b84627396fb1edd204da07b2b974370c2907d
hawa130 <handanwanga@hotmail.com>
2025-03-25 06:35:44 +0800

    feat: enhance FormItem to conditionally render children based on passChild prop

--- components/ui/form.tsx
+++ components/ui/form.tsx
@@ -69,12 +69,18 @@ type FormItemContextValue = {
 
 const FormItemContext = React.createContext<FormItemContextValue>({} as FormItemContextValue)
 
-function FormItem({ className, ...props }: React.ComponentProps<'div'>) {
+function FormItem({ className, children, passChild, ...props }: React.ComponentProps<'div'> & { passChild?: boolean }) {
   const id = React.useId()
 
   return (
     <FormItemContext.Provider value={{ id }}>
-      <div data-slot="form-item" className={cn('grid gap-2', className)} {...props} />
+      {passChild ? (
+        <>{children}</>
+      ) : (
+        <div data-slot="form-item" className={cn('grid gap-2', className)} {...props}>
+          {children}
+        </div>
+      )}
     </FormItemContext.Provider>
   )
 }
