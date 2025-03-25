1fe9b5121410ca06bd1f505ebce09c91fefa3f89
hawa130 <handanwanga@hotmail.com>
2025-03-25 21:40:26 +0800

    feat: add CheckConnectionButton component for validating connection info

---
+++ app/connect-info/check-connection-button.tsx
@@ -0,0 +1,40 @@
+import { Button } from '@/components/ui/button'
+import { validateConnectionInfo } from '@/app/connect-info/utils'
+import { useGlobalStore } from '@/stores'
+
+export function CheckConnectionButton() {
+  const connectMode = useGlobalStore((s) => s.connectMode)
+  const sshHosts = useGlobalStore((s) => s.sshHosts)
+  const bmcHosts = useGlobalStore((s) => s.bmcHosts)
+  const defaultCredentials = useGlobalStore((s) => s.defaultCredentials)
+  const data = { sshHosts, bmcHosts, defaultCredentials }
+
+  return (
+    <Button
+      onClick={() => {
+        switch (connectMode) {
+          case 'bmc': {
+            const result = validateConnectionInfo(data, 'bmc')
+            if (!result.success) {
+              console.log(result.error)
+              return
+            }
+            console.log(result.data)
+            break
+          }
+          case 'ssh': {
+            const result = validateConnectionInfo(data, 'ssh')
+            if (!result.success) {
+              console.log(result.error)
+              return
+            }
+            console.log(result.data)
+            break
+          }
+        }
+      }}
+    >
+      检查连接
+    </Button>
+  )
+}
