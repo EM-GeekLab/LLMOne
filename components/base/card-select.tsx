3700ebf4a867d04ce7a2a9dd4742a422d77fbdbd
hawa130 <handanwanga@hotmail.com>
2025-03-31 10:10:13 +0800

    feat: enhance card-select component with focus-visible styles

--- components/base/card-select.tsx
+++ components/base/card-select.tsx
@@ -11,7 +11,7 @@ export function CardSelectItem({ className, ...props }: React.ComponentProps<typ
   return (
     <RadioGroupPrimitive.Item
       className={cn(
-        'group bg-card aria-checked:border-primary aria-checked:bg-primary/5 hover-enabled:border-accent-foreground/25 hover-enabled:bg-accent disabled-all:opacity-50 disabled-all:grayscale-100 not-disabled-all:pointer-events-auto relative flex flex-col items-start overflow-hidden rounded-lg border p-5 text-left transition-colors disabled:pointer-events-none',
+        'group bg-card aria-checked:border-primary aria-checked:bg-primary/5 hover-enabled:border-accent-foreground/25 hover-enabled:bg-accent disabled-all:opacity-50 disabled-all:grayscale-100 not-disabled-all:pointer-events-auto focus-visible:border-ring focus-visible:ring-ring/50 relative flex flex-col items-start overflow-hidden rounded-lg border p-5 text-left transition-colors outline-none focus-visible:ring-[3px] disabled:pointer-events-none',
         className,
       )}
       {...props}
