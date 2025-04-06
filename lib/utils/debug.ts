8d5fe83797855907cffae08e1b54600a3eb8ca34
hawa130 <handanwanga@hotmail.com>
2025-04-07 07:58:29 +0800

    feat: add sleep utility function

--- lib/utils/debug.ts
+++ lib/utils/debug.ts
@@ -6,3 +6,7 @@ export function debug<T>(v: T, tag?: string): T {
   }
   return v
 }
+
+export async function sleep(ms: number): Promise<void> {
+  return new Promise((resolve) => setTimeout(resolve, ms))
+}
