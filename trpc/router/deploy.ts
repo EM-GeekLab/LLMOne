0a413eb9821382ef775b81cc18fb972ab8e12b30
hawa130 <handanwanga@hotmail.com>
2025-04-07 10:52:32 +0800

    feat: enhance deploy router with installOne and retryFromStep mutations

--- trpc/router/deploy.ts
+++ trpc/router/deploy.ts
@@ -1,7 +1,22 @@
-import { MxdManager } from '@/lib/metalx'
-import { installConfigSchema } from '@/app/host-info/schemas'
+import { TRPCError } from '@trpc/server'
+import { z } from 'zod'
+
+import { installSteps, MxdManager } from '@/lib/metalx'
+import { installConfigSchema, installOneConfigSchema } from '@/app/host-info/schemas'
 import { baseProcedure, createRouter } from '@/trpc/init'
 import { readOsInfoAbsolute } from '@/trpc/router/resource-utils'
+
+let mxd: MxdManager | null = null
+
+function getMxdManager() {
+  if (!mxd) {
+    throw new TRPCError({
+      code: 'BAD_REQUEST',
+      message: '需要安装操作系统',
+    })
+  }
+  return mxd
+}
 
 export const deployRouter = createRouter({
   os: {
@@ -10,12 +25,30 @@ export const deployRouter = createRouter({
     }) {
       try {
         const osInfo = await readOsInfoAbsolute(osInfoPath)
-        const mxd = await MxdManager.create({ hosts, account, network, systemImagePath: osInfo.file })
+        mxd = await MxdManager.create({ hosts, account, network, systemImagePath: osInfo.file })
         yield* mxd.installAll()
       } catch (err) {
         console.log(err)
         throw err
       }
     }),
+    installOne: baseProcedure.input(installOneConfigSchema).mutation(async function* ({
+      input: { host, account, network, osInfoPath },
+    }) {
+      try {
+        const osInfo = await readOsInfoAbsolute(osInfoPath)
+        mxd = await MxdManager.create({ hosts: [host], account, network, systemImagePath: osInfo.file })
+        yield* mxd.installOne(0)
+      } catch (err) {
+        console.log(err)
+        throw err
+      }
+    }),
+    retryFromStep: baseProcedure
+      .input(z.object({ host: z.string(), step: z.enum(installSteps).nullable() }))
+      .mutation(async function* ({ input: { host, step } }) {
+        const mxd = getMxdManager()
+        yield* mxd.installHostFromStep(host, step)
+      }),
   },
 })
