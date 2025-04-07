becdfbe96afc80f2ba9a459bdcddc2846bc37ec8
hawa130 <handanwanga@hotmail.com>
2025-04-07 08:01:34 +0800

    feat: enhance Progress component with variant

--- components/ui/progress.tsx
+++ components/ui/progress.tsx
@@ -5,16 +5,28 @@ import * as ProgressPrimitive from '@radix-ui/react-progress'
 
 import { cn } from '@/lib/utils'
 
-function Progress({ className, value, ...props }: React.ComponentProps<typeof ProgressPrimitive.Root>) {
+function Progress({
+  className,
+  value,
+  variant = 'primary',
+  ...props
+}: React.ComponentProps<typeof ProgressPrimitive.Root> & {
+  variant?: 'primary' | 'destructive' | 'success'
+}) {
   return (
     <ProgressPrimitive.Root
       data-slot="progress"
-      className={cn('bg-primary/20 relative h-2 w-full overflow-hidden rounded-full', className)}
+      data-variant={variant}
+      className={cn(
+        'data-[variant=primary]:bg-primary/20 data-[variant=destructive]:bg-destructive/20 data-[variant=success]:bg-success/20 relative h-2 w-full overflow-hidden rounded-full',
+        className,
+      )}
       {...props}
     >
       <ProgressPrimitive.Indicator
         data-slot="progress-indicator"
-        className="bg-primary h-full w-full flex-1 transition-all"
+        data-variant={variant}
+        className="data-[variant=primary]:bg-primary data-[variant=destructive]:bg-destructive data-[variant=success]:bg-success h-full w-full flex-1 transition-all"
         style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
       />
     </ProgressPrimitive.Root>
