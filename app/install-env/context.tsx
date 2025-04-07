17d8e5cb6d12eabe07350176cfd9be0c20a749d4
hawa130 <handanwanga@hotmail.com>
2025-04-07 10:55:01 +0800

    feat: add retry functionality

--- app/install-env/context.tsx
+++ app/install-env/context.tsx
@@ -4,9 +4,10 @@ import { ReactNode } from 'react'
 import { useMutation } from '@tanstack/react-query'
 import { toast } from 'sonner'
 
+import { InstallStep } from '@/lib/metalx'
 import { createSafeContext } from '@/lib/react/create-safe-context'
-import { installConfigSchema } from '@/app/host-info/schemas'
-import { formatProgress } from '@/app/install-env/utils'
+import { installConfigSchema } from '@/app/install-env/schemas'
+import { formatProgress, progressText } from '@/app/install-env/utils'
 import { useGlobalStoreApi } from '@/stores'
 import { useLocalStore } from '@/stores/local-store-provider'
 import { useTRPCClient } from '@/trpc/client'
@@ -19,6 +20,7 @@ const BmcLocalInstallContext = createSafeContext<{
   start: () => void
   status: MutationStatus
   isPending: boolean
+  retry: (hostId: string, step: InstallStep) => void
 }>()
 
 export function BmcLocalInstallProvider({ children }: { children: ReactNode }) {
@@ -28,7 +30,7 @@ export function BmcLocalInstallProvider({ children }: { children: ReactNode }) {
   const trpc = useTRPCClient()
 
   const mutation = useMutation({
-    mutationFn: async function () {
+    mutationFn: async () => {
       const { hosts, account, network } = storeApi.getState().hostConfig
       const osInfoPath = storeApi.getState().osInfoPath
       const input = installConfigSchema.parse({ hosts: Array.from(hosts.values()), account, network, osInfoPath })
@@ -46,11 +48,36 @@ export function BmcLocalInstallProvider({ children }: { children: ReactNode }) {
     retry: false,
   })
 
+  const retryMutation = useMutation({
+    mutationFn: async ({ hostId, step }: { hostId: string; step: InstallStep }) => {
+      addLog(hostId, { type: 'info', log: `从${progressText(step)}重试`, time: new Date() })
+      for await (const result of await trpc.deploy.os.retryFromStep.mutate(
+        { host: hostId, step },
+        { context: { stream: true } },
+      )) {
+        setProgress(result.host.id, result)
+        addLog(result.host.id, formatProgress(result))
+      }
+    },
+    onError: (error) => {
+      console.error(error)
+      toast.error('安装过程中出现错误', {
+        description: error.message,
+      })
+    },
+    retry: false,
+  })
+
   usePreventUnload(mutation.isPending)
 
   return (
     <BmcLocalInstallContext.Provider
-      value={{ start: () => mutation.mutate(), status: mutation.status, isPending: mutation.isPending }}
+      value={{
+        start: () => mutation.mutate(),
+        status: mutation.status,
+        isPending: mutation.isPending,
+        retry: (hostId, step) => retryMutation.mutate({ hostId, step }),
+      }}
     >
       {children}
     </BmcLocalInstallContext.Provider>
