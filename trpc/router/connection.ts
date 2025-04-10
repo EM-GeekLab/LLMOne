4eeb32c779203defb46e360f10d74b371e389a3b
hawa130 <handanwanga@hotmail.com>
2025-04-11 01:51:40 +0800

    feat: use mxc for boot file url retrieval

--- trpc/router/connection.ts
+++ trpc/router/connection.ts
@@ -8,10 +8,9 @@ import { autoDetect, iBMCRedfishClient, iDRACRedfishClient } from 'redfish-clien
 import { BmcClients } from '@/lib/bmc-clients'
 import { mxc } from '@/lib/metalx'
 import { runMxc } from '@/lib/metalx/mxc'
-import { selectIpInSameSubnet } from '@/lib/network'
 import { z } from '@/lib/zod'
 import { BmcFinalConnectionInfo, bmcHostsListSchema, SshFinalConnectionInfo } from '@/app/connect-info/schemas'
-import { HostExtraInfo } from '@/sdk/mxlite'
+import { HostExtraInfo } from '@/sdk/mxlite/types'
 import { baseProcedure, createRouter } from '@/trpc/init'
 
 import { getDefaultArchitecture } from './bmc-utils'
@@ -45,25 +44,26 @@ export const connectionRouter = createRouter({
           const architecture = await getDefaultArchitecture(bmcClients, true)
           const bootstrapPath = await getBootstrapPath(manifestPath, architecture)
           const bootstrapFile = basename(bootstrapPath)
-          const bootstrapDir = dirname(bootstrapPath)
-          await runMxc(bootstrapDir)
 
-          const bootUrl = new URL(mxc.endpoint)
-          bootUrl.pathname = `/static/${bootstrapFile}`
+          if (process.env.MXC_EXECUTABLE) {
+            // 如果指定了 MXC_EXECUTABLE 环境变量，则使用本地的 mxd 可执行文件
+            const bootstrapDir = dirname(bootstrapPath)
+            await runMxc(bootstrapDir)
+          }
 
           const errors = await bmcClients.map(async ({ defaultId, ip, client }) => {
-            const localIp = selectIpInSameSubnet(ip)
-            if (!localIp) {
+            const [res, status] = await mxc.urlSubByIp(`static/bootstrap/${bootstrapFile}`, ip)
+            if (status >= 400 || !res.ok) {
               return new TRPCError({
-                code: 'BAD_REQUEST',
-                message: `本机与 ${ip} 不在同一子网`,
+                code: 'INTERNAL_SERVER_ERROR',
+                message: `${ip} 引导文件服务失败`,
               })
             }
 
-            bootUrl.hostname = localIp.address
+            const { urls } = res
 
             try {
-              const { status } = await client.bootVirtualMedia(bootUrl.toString(), defaultId)
+              const { status } = await client.bootVirtualMedia(urls[0], defaultId)
               if (!status)
                 return new TRPCError({
                   code: 'INTERNAL_SERVER_ERROR',
