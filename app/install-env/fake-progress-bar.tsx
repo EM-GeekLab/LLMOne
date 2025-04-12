2862bfc965430fbed30181a29e783ad49913e6df
hawa130 <handanwanga@hotmail.com>
2025-04-12 21:48:39 +0800

    feat: update progress bar components props type

--- app/install-env/fake-progress-bar.tsx
+++ app/install-env/fake-progress-bar.tsx
@@ -1,14 +1,17 @@
 import { ComponentProps } from 'react'
 import { CheckIcon, XIcon } from 'lucide-react'
 
-import { InstallProgressBase } from '@/lib/metalx'
 import { useFakeProgress } from '@/lib/progress'
 import { Progress } from '@/components/ui/progress'
 import { RingProgress } from '@/components/ui/ring-progress'
 
-function useInstallProgress<T extends string | null>(
-  progress?: InstallProgressBase<T>,
-): {
+type FakeProgressProps = {
+  from: number
+  to: number
+  ok: boolean
+}
+
+function useInstallProgress(progress?: FakeProgressProps): {
   value: number
   variant: 'destructive' | 'success' | undefined
 } {
@@ -27,21 +30,21 @@ function useInstallProgress<T extends string | null>(
   }
 }
 
-export function FakeProgressBar<T extends string | null>({
+export function FakeProgressBar({
   progress,
   ...props
 }: {
-  progress?: InstallProgressBase<T> & ComponentProps<typeof Progress>
+  progress?: FakeProgressProps & ComponentProps<typeof Progress>
 }) {
   const { value, variant } = useInstallProgress(progress)
   return <Progress value={value} variant={variant} {...props} />
 }
 
-export function FakeRingProgressBar<T extends string | null>({
+export function FakeRingProgressBar({
   progress,
   ...props
 }: {
-  progress?: InstallProgressBase<T>
+  progress?: FakeProgressProps
 } & ComponentProps<typeof RingProgress>) {
   const { value, variant } = useInstallProgress(progress)
 
