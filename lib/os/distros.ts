8ac694654e61ff2d403e0f1b2372d7ec4ee25f73
hawa130 <handanwanga@hotmail.com>
2025-04-17 19:25:38 +0800

    feat: update architectures to include only ARM64 and x86_64

--- lib/os/distros.ts
+++ lib/os/distros.ts
@@ -1,20 +1,21 @@
 export const distributions = ['openEuler', 'ubuntu', 'fedora', 'debian'] as const
 export type OsDistribution = (typeof distributions)[number]
 
-export const architectures = [
-  'ARM64',
-  'ARM32',
-  'LoongArch64',
-  'RISC-V',
-  'x86_64',
-  'RISC-V',
-  'ppc64le',
-  's390x',
-  'armel',
-  'armhf',
-  'i386',
-  'mips64el',
-  'mips',
-  'mipsel',
-] as const
+// export const architectures = [
+//   'ARM64',
+//   'ARM32',
+//   'LoongArch64',
+//   'RISC-V',
+//   'x86_64',
+//   'RISC-V',
+//   'ppc64le',
+//   's390x',
+//   'armel',
+//   'armhf',
+//   'i386',
+//   'mips64el',
+//   'mips',
+//   'mipsel',
+// ] as const
+export const architectures = ['ARM64', 'x86_64'] as const
 export type OsArchitecture = (typeof architectures)[number]
