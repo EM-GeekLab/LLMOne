9970c9c283976acb103375534ad2fdd5fc09d597
hawa130 <handanwanga@hotmail.com>
2025-04-19 17:10:41 +0800

    chore: add logging for TRPC server startup

--- trpc/index.ts
+++ trpc/index.ts
@@ -1,6 +1,9 @@
+import { logger } from '@/lib/logger'
 import { createTRPCServer } from '@/trpc/create-server'
 
 const portConfig = Number(process.env.TRPC_SERVER_PORT)
 const port = isNaN(portConfig) ? 3008 : portConfig
 
-createTRPCServer().listen(port)
+const srv = createTRPCServer().listen(port)
+
+logger.info(srv.address(), 'trpc server started')
