8855411089e209f5625eca3a78c36acb354a65d8
hawa130 <handanwanga@hotmail.com>
2025-04-25 01:42:20 +0800

    feat: pretty print build log

--- scripts/print-build.ts
+++ scripts/print-build.ts
@@ -1,20 +1,12 @@
 import { basename } from 'node:path'
 
-import { BuildOutput, color as bunColor } from 'bun'
+import { BuildOutput } from 'bun'
+import { blueBright, white, yellow, yellowBright } from 'colorette'
 import { filesize } from 'filesize'
 
-const colors = {
-  blue: bunColor('skyblue', 'ansi'),
-  yellow: bunColor('yellow', 'ansi'),
-  darkYellow: bunColor('darkkhaki', 'ansi'),
-  gray: bunColor('gray', 'ansi'),
-  lightgray: bunColor('darkgray', 'ansi'),
-  reset: '\u001b[0m',
-}
-
-function withColor(text: string, color?: string | null) {
+function withColor(text: string, color?: (text: string | number) => string) {
   if (color) {
-    return `${color}${text}${colors.reset}`
+    return color(text)
   }
   return text
 }
@@ -23,12 +15,12 @@ export function printBuild(result: BuildOutput) {
   console.group()
   for (const artifact of result.outputs) {
     console.log(
-      withColor(basename(artifact.path).padEnd(16), artifact.kind === 'sourcemap' ? colors.lightgray : colors.blue),
+      withColor(basename(artifact.path).padEnd(16), artifact.kind === 'sourcemap' ? white : blueBright),
       withColor(
         filesize(artifact.size, { standard: 'jedec' }).padEnd(10),
-        artifact.kind === 'sourcemap' ? colors.lightgray : colors.yellow,
+        artifact.kind === 'sourcemap' ? white : yellowBright,
       ),
-      withColor(`(${artifact.kind})`, artifact.kind === 'entry-point' ? colors.darkYellow : colors.gray),
+      withColor(`(${artifact.kind})`, artifact.kind === 'entry-point' ? yellow : white),
     )
   }
   console.groupEnd()
